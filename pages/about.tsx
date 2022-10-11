94d0ea97eb8c8dda1255d8f70966716d6b081b03
MasterKiri13 <leoneleduardomadrid@hotmail.com>
2022-10-10 22:49:13 -0600

    updated image src

--- pages/about.tsx
+++ pages/about.tsx
@@ -6,7 +6,7 @@ export default function AboutPage() {
       <p>About us</p>
       <p>Hello Puta!!</p>
       <p>chupala Santa</p>
-      <img src="../images/Santa.jpg" alt="Santa Claus"/>
+      <img src="https://scontent.fntr4-1.fna.fbcdn.net/v/t1.6435-9/33713222_1682413791808224_5067021189675745280_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=174925&_nc_ohc=xyAsvmYdHp4AX8lG1Zz&_nc_ht=scontent.fntr4-1.fna&oh=00_AT8vfhhN_T_bcAZgemZxdsMWONj2TvuK0ril92b8ecDjkA&oe=636A1C79" alt="Santa Claus"/>
       <div>
         <Link href="./Greet/greet">
           <a>Greet</a>
